10a8f949777f649423ebd4cd781fe0c1a5d09d10
easigregory <fdsleeper@gmail.com>
2017-08-12 02:20:13 -0500

    Adding a few more tests for different manager, qa and developer combinations

--- src/test/AllocationTest.java
+++ src/test/AllocationTest.java
@@ -5,26 +5,55 @@ import main.java.domain.Manager;
 import main.java.domain.QA;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class AllocationTest {
 
     @Test
     public void verifyQaAllocation() {
         QA qa = new QA();
 
-        assert qa.getAllocation() == 500;
+        assertEquals(qa.getAllocation(), 500);
     }
 
     @Test
     public void verifyDeveloperAllocation() {
         Developer developer = new Developer();
 
-        assert developer.getAllocation() == 1000;
+        assertEquals(developer.getAllocation(), 1000);
     }
 
     @Test
     public void verifyManagerAllocation() {
         Manager manager = new Manager();
 
-        assert manager.getAllocation() == 300;
+        assertEquals(manager.getAllocation(), 300);
+    }
+
+    @Test
+    public void verifyManagerWithQaAllocation() {
+        Manager manager = new Manager();
+        manager.addEmployee(new QA());
+
+        assertEquals(manager.getTeamAllocation(), 800);
+    }
+
+    @Test
+    public void verifyManagerWithQaAndDeveloperAllocation() {
+        Manager manager = new Manager();
+        manager.addEmployee(new QA());
+        manager.addEmployee(new Developer());
+
+        assertEquals(manager.getTeamAllocation(), 1800);
+    }
+
+    @Test
+    public void verifyManagerWithManagerQaAndDeveloperAllocation() {
+        Manager manager = new Manager();
+        manager.addEmployee(new Manager());
+        manager.addEmployee(new QA());
+        manager.addEmployee(new Developer());
+
+        assertEquals(manager.getTeamAllocation(), 2100);
     }
 }
